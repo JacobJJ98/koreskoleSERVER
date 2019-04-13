9dd532e925c235e0875bfafd88f7c9534ef1ddbc
alkj <s165477@student.dtu.dk>
2019-04-13 15:59:58 +0200

    den kører nu på jacobs server

--- src/server/Main.java
+++ src/server/Main.java
@@ -7,12 +7,14 @@ import java.sql.ResultSet;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		try {
-		java.rmi.registry.LocateRegistry.createRegistry(1235); // start i server-JVM                
-		KøreskolePriserImplementering ki = new KøreskolePriserImplementering(); 
-		Naming.rebind("rmi://localhost:1235/koereskolepriser", ki);
-		System.out.println("serveren er startet");
+			java.rmi.registry.LocateRegistry.createRegistry(5478); // start i server-JVM
+			KøreskolePriserImplementering ki = new KøreskolePriserImplementering();
+//			Naming.rebind("rmi://localhost:1235/koereskolepriser", ki);
+			Naming.rebind("rmi://130.225.170.204:5478/koereskolepriser", ki);
+
+			System.out.println("serveren er startet");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
