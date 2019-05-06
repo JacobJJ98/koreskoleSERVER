62b5c63dbe946ba146803893cd3cef0d6e96e30a
Alexander Kjeldsen <s165477@student.dtu.dk>
2019-05-06 11:56:03 +0200

    højere stabilitet mht brugeraut modul

--- src/server/Main.java
+++ src/server/Main.java
@@ -12,11 +12,10 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			java.rmi.registry.LocateRegistry.createRegistry(1235); // start i server-JVM
+			java.rmi.registry.LocateRegistry.createRegistry(5500); // start i server-JVM
 			KøreskolePriserImplementering ki = new KøreskolePriserImplementering();
-			Naming.rebind("rmi://localhost:1235/koereskolepriser", ki);
-//			Naming.rebind("rmi://dist.saluton.dk:5478/koereskolepriser", ki);
-
+//			Naming.rebind("rmi://localhost:1235/koereskolepriser", ki);
+			Naming.rebind("rmi://dist.saluton.dk:5500/koereskolepriser", ki);
 			System.out.println("serveren er startet");
 		} catch (Exception e) {
 			e.printStackTrace();
